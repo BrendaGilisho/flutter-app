28c3274d36114c05dbf7478e588cab56079d4bfb
Brenda Gilisho <57228014+BrendaGilisho@users.noreply.github.com>
2022-02-11 09:46:17 +0200

    Update welcome_screen.dart

--- lib/Screens/welcome_screen.dart
+++ lib/Screens/welcome_screen.dart
@@ -11,19 +11,13 @@ class MyCustomForm extends StatefulWidget {
   }
 }
 
-// Create a corresponding State class.
-// This class holds data related to the form.
-class MyCustomFormState extends State<MyCustomForm> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
 
-  // Note: This is a GlobalKey<FormState>,
-  // not a GlobalKey<MyCustomFormState>.
+class MyCustomFormState extends State<MyCustomForm> {
+  
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
     return Scaffold(
       body: Form(
         key: _formKey,
@@ -31,7 +25,6 @@ class MyCustomFormState extends State<MyCustomForm> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             TextFormField(
-              // The validator receives the text that the user has entered.
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return 'Please enter some text';
@@ -45,8 +38,7 @@ class MyCustomFormState extends State<MyCustomForm> {
                 onPressed: () {
                   // Validate returns true if the form is valid, or false otherwise.
                   if (_formKey.currentState!.validate()) {
-                    // If the form is valid, display a snackbar. In the real world,
-                    // you'd often call a server or save the information in a database.
+                   
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(content: Text('Processing Data')),
                     );
